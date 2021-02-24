2cd1eedaeca08338eebe135221256f1dca71a8b6
Lauren Hutchinson <laurenhutchinson@Laurens-MacBook-Pro.local>
2021-02-24 08:45:00 -0800

    file structure

--- employee-directory/src/utils/API/API.js
+++
@@ -1,9 +0,0 @@
-import axios from "axios";
-const BASEURL = "https://randomuser.me/api/?results=10";
-
-export default {
-  search: function () {
-    console.log(BASEURL);
-    return axios.get(BASEURL);
-  },
-};
